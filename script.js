891e7b0c34ce5009fb891354d7071d6168a9de49
andersonsouzacardoso <andersonfdl@gmail.com>
2022-10-16 21:30:45 -0300

    Minha Lista

---
+++ script.js
@@ -0,0 +1,6 @@
+function adicionar(){
+    let text = document.getElementById("texto").value
+    let list = document.getElementById("lista").innerHTML
+    list += "<li>" +text+ "</li>"
+    document.getElementById("lista").innerHTML = list
+}
